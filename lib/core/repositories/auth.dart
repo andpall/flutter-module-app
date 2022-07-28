7d4ba82f75bba5426f009b3baa32d1b141b59169
andpall <andpall5@gmail.com>
2022-07-28 16:08:52 +0300

    add state provider for auth

--- lib/core/repositories/auth.dart
+++ lib/core/repositories/auth.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:flutter_mod_app/core/models/auth_data.dart';
 
 class AuthRepository {
   static final AuthRepository _authRepository = AuthRepository._internal();
@@ -13,20 +13,19 @@ class AuthRepository {
   void listenUser() {
     FirebaseAuth.instance.authStateChanges().listen(
           (User? user) {},
-    );
+        );
   }
 
   User? getUser() {
     return FirebaseAuth.instance.currentUser;
   }
 
-  Future<void> signWithMail(email, password) async {
+  Future<void> signWithMail(AuthData data) async {
     try {
-      await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: data.username, password: data.password);
     } on FirebaseAuthException {
       rethrow;
     }
   }
-
 }

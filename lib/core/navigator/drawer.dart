393c07b3fad2a9ef482dc4e80fd4168d4b5aa24c
andpall <andpall5@gmail.com>
2022-08-10 13:46:11 +0300

    add color to selected item

--- lib/core/navigator/drawer.dart
+++ lib/core/navigator/drawer.dart
@@ -19,7 +19,11 @@ class _DrawerNavigatorState extends State<DrawerNavigator> {
   String? _email;
   String? _nick;
 
-  int _selectedIndex = 0;
+  late double screenWidth;
+  late double drawerWidth = screenWidth * 0.7;
+  double imageRadius = 60;
+
+  static int _selectedIndex = 0;
 
   final List<_DrawerItem> drawerItems = [
     _DrawerItem(route: AppRoutes.homeRoute, label: "Home"),
@@ -27,66 +31,79 @@ class _DrawerNavigatorState extends State<DrawerNavigator> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    screenWidth = MediaQuery.of(context).size.width;
+
     var profileData = Provider.of<ProfileStateProvider>(context).getProfileData;
     _profileAvatar = profileData?.avatarUrl;
     _email = profileData?.email;
     _nick = profileData?.nick;
   }
 
+  void tileCallback(index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-
-    return Drawer(
-      width: screenWidth * 0.7,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(bottomRight: Radius.circular(20)),
-      ),
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: [
-          SizedBox(
-            height: 240,
-            child: UserAccountsDrawerHeader(
-              decoration: const BoxDecoration(color: Colors.transparent),
-              currentAccountPictureSize: const Size.square(100),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                backgroundImage: NetworkImage(_profileAvatar!),
-              ),
-              accountEmail: Text(
-                _email ?? "",
-                style: const TextStyle(color: Colors.red),
-              ),
-              accountName: Text(_nick ?? "",
-                  style: const TextStyle(color: AppColors.disabledBtn)),
+    return SafeArea(
+      child: Drawer(
+        width: drawerWidth,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(20)),
+        ),
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: CircleAvatar(
+                        radius: 60,
+                        backgroundImage: NetworkImage(_profileAvatar!)),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    _nick ?? "",
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    _email ?? "",
+                  ),
+                )
+              ],
             ),
-          ),
-          Container(
-            // margin: const EdgeInsets.only(right: 80),
-            padding: EdgeInsets.only(right: screenWidth * 0.7 - 120 - 20),
-            child: CircleAvatar(
-                radius: 60, backgroundImage: NetworkImage(_profileAvatar!)),
-          ),
-          Text(
-            _nick ?? "",
-          ),
-          Text(
-            _email ?? "",
-          ),
-          ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              padding: const EdgeInsets.all(4.0),
-              itemCount: drawerItems.length,
-              itemBuilder: (context, index) {
-                return _CustomListTile(
-                    drawerItems[index].route, drawerItems[index].label);
-              }),
-        ],
+            ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                padding: const EdgeInsets.all(4.0),
+                itemCount: drawerItems.length,
+                itemBuilder: (context, index) {
+                  return _CustomListTile(
+                      drawerItems[index].route,
+                      drawerItems[index].label,
+                      null,
+                      index == _selectedIndex,
+                      () => tileCallback(index));
+                }),
+          ],
+        ),
       ),
     );
   }
@@ -96,18 +113,25 @@ class _CustomListTile extends StatelessWidget {
   final String? title;
   final String routeName;
   final Icon? icon;
+  final bool selected;
+  final VoidCallback? callback;
 
-  const _CustomListTile(this.routeName, [this.title, this.icon]);
+  const _CustomListTile(this.routeName,
+      [this.title, this.icon, this.selected = false, this.callback]);
 
   @override
   Widget build(BuildContext context) {
     void _onTap() {
+      if (callback != null) callback!();
       Navigator.pop(context);
       Navigator.of(context).pushNamed(routeName);
     }
 
     return ListTile(
-      title: Text(title ?? routeName),
+      title: Text(
+        title ?? routeName,
+        style: selected ? const TextStyle(color: AppColors.mainColor) : null,
+      ),
       onTap: _onTap,
     );
   }
